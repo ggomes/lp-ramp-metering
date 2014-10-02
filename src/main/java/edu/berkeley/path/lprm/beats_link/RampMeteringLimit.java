564ade81f2130baf190334abbb8b9c967b76de85
Gabriel Gomes <gomes@path.berkeley.edu>
2014-10-02 15:01:17 -0700

    delete beats_link

--- src/main/java/edu/berkeley/path/lprm/beats_link/RampMeteringLimit.java
+++
@@ -1,9 +0,0 @@
-package edu.berkeley.path.lprm.beats_link;
-
-import edu.berkeley.path.lprm.jaxb.Actuator;
-
-public class RampMeteringLimit {
-    public double min_rate; // flux in relation to max rate of link
-    public double max_rate; // flux in relation to max rate of link
-    public Actuator actuator;
-}
